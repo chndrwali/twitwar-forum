2cb231ad45a6f5ad2d4e848f8aaa179e77742055
chndrwali <chndrwali@gmail.com>
2023-07-13 15:33:38 +0700

    fix reacthooks eslint, add action and reducer for isPreload and thread

---
+++ src/states/thread/action.js
@@ -0,0 +1,74 @@
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+
+const ActionType = {
+  RECEIVE_THREADS: 'RECEIVE_THREADS',
+  ADD_THREAD: 'ADD_THREAD',
+  TOGGLE_VOTE_THREAD: 'TOGGLE_VOTE_THREAD',
+};
+
+function receiveThreadsActionCreator(threads) {
+  return {
+    type: ActionType.RECEIVE_THREADS,
+    payload: {
+      threads,
+    },
+  };
+}
+
+function addThreadActionCreator(thread) {
+  return {
+    type: ActionType.ADD_THREAD,
+    payload: {
+      thread,
+    },
+  };
+}
+
+function toggleVoteThreadActionCreator({ threadId, userId }) {
+  return {
+    type: ActionType.TOGGLE_VOTE_THREAD,
+    payload: {
+      threadId,
+      userId,
+    },
+  };
+}
+
+function asyncAddThread({ title, body, category = '' }) {
+  return async (dispatch) => {
+    dispatch(showLoading());
+    try {
+      const thread = await api.createThread({ title, body, category });
+      dispatch(addThreadActionCreator(thread));
+    } catch (error) {
+      alert(error.message);
+    }
+    dispatch(hideLoading());
+  };
+}
+
+function asyncToogleVoteThread(threadId) {
+  return async (dispatch, getState) => {
+    dispatch(showLoading());
+    const { authUser } = getState();
+    dispatch(toggleVoteThreadActionCreator({ threadId, userId: authUser.id }));
+
+    try {
+      await api.toggleUpVoteThreads(threadId);
+    } catch (error) {
+      alert(error.message);
+      dispatch(toggleVoteThreadActionCreator({ threadId, userId: authUser.id }));
+    }
+    dispatch(hideLoading());
+  };
+}
+
+export {
+  ActionType,
+  receiveThreadsActionCreator,
+  addThreadActionCreator,
+  toggleVoteThreadActionCreator,
+  asyncAddThread,
+  asyncToogleVoteThread,
+};
